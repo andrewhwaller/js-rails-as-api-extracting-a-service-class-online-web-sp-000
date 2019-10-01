e4e5b36de577c3527f2723bbcb8aff62d4781832
Andrew Waller <andrewhwaller@gmail.com>
2019-10-01 19:28:41 +0000

    Automatically backed up by Learn

--- app/services/sighting_serializer.rb
+++ app/services/sighting_serializer.rb
@@ -5,10 +5,10 @@ class SightingSerializer
   end
 
   def to_serialized_json
-  @sighting.to_json(:include => {
-    :bird => {:only => [:name, :species]},
-    :location => {:only => [:latitude, :longitude]}
-  }, :except => [:updated_at])
-end
+    @sighting.to_json(:include => {
+      :bird => {:only => [:name, :species]},
+      :location => {:only => [:latitude, :longitude]}
+      }, :except => [:updated_at])
+    end
 
 end
